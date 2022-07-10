5669f0e1325a0f6187f91586392b66a3406801c9
Kyuho Kim <ekyuho@gmail.com>
2022-07-11 00:05:41 +0900

    0711 0005

--- graph.py
+++ graph.py
@@ -3,7 +3,14 @@ import json
 def mygraph(d):
     with open("graph.html") as f: format=f.read()
     s=''
+    s2=''
+    k=0
     for x in d:
         s+= f",['{x[0]}',{x[1]}]"
+        s2+=f" {x[1]}"
+        k+=1
     #print(s)
-    return format.replace('<%DATA%>', s)
+    s = format.replace('<%DATA%>', s)
+    s = s.replace('<%RAWDATA%>', s2)
+    s = s.replace('<%LENGTH%>', str(k))
+    return s
