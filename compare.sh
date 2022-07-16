5a1e6eb3dab9d62e032187a7a4a5413fadac5e0d
Kyuho Kim <ekyuho@gmail.com>
2022-07-16 19:46:20 +0900

    0716 1947

---
+++ compare.sh
@@ -0,0 +1,17 @@
+diff ../GB2.0/actuate.py actuate.py
+diff ../GB2.0/build_package.py build_package.py
+diff ../GB2.0/camera.py camera.py
+diff ../GB2.0/Client_Data_Saving.py Client_Data_Saving.py
+diff ../GB2.0/create.py create.py
+diff ../GB2.0/make_oneM2M_resource.py make_oneM2M_resource.py
+diff ../GB2.0/query_all_resource.py query_all_resource.py
+diff ../GB2.0/query_ci.py query_ci.py
+diff ../GB2.0/read_resource.py read_resource.py
+diff ../GB2.0/RepeatedTimer.py RepeatedTimer.py
+diff ../GB2.0/savedData.py savedData.py
+diff ../GB2.0/Server_Data_Sending.py Server_Data_Sending.py
+diff ../GB2.0/state.py state.py
+diff ../GB2.0/versionup.py versionup.py
+diff ../GB2.0/graph.py graph.py
+diff ../GB2.0/graph.html graph.html
+diff ../GB2.0/myserial.py myserial.py
