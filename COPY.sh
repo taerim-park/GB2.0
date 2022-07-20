3301d12b1ffee9c4c672a4cda21d96d71edcc95e
Kyuho Kim <ekyuho@gmail.com>
2022-07-21 01:37:31 +0900

    0721 0137

---
+++ COPY.sh
@@ -0,0 +1,14 @@
+# run   sh -x CPOY.sh
+cp ../GB2.0/camera.py camera.py
+cp ../GB2.0/Client_Data_Saving.py Client_Data_Saving.py
+cp ../GB2.0/create.py create.py
+cp ../GB2.0/make_oneM2M_resource.py make_oneM2M_resource.py
+cp ../GB2.0/RepeatedTimer.py RepeatedTimer.py
+cp ../GB2.0/savedData.py savedData.py
+cp ../GB2.0/Server_Data_Sending.py Server_Data_Sending.py
+cp ../GB2.0/state.py state.py
+cp ../GB2.0/versionup.py versionup.py
+cp ../GB2.0/graph.py graph.py
+cp ../GB2.0/graph.html graph.html
+cp ../GB2.0/myserial.py myserial.py
+
